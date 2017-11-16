7a2644e550fe59d8a12a8c19cabbaf06dc7c6d96
Karl M. Mobe <kmobe@courthousenews.com>
2017-11-16 18:07:17 +0800

    resolve for merging

---
+++ Server/SBO-1028/SBOClient/Controllers/SboControllers/JournalController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SBOClient.Controllers.SboControllers
+{
+    public class JournalController : ApiController
+    {
+
+    }
+}
